0206308dc0dfc14a2bdce14e8491ce8582ddfd95
Larissa <larissamoreiracarvalho@gmail.com>
2024-12-12 22:37:25 -0300

    fix/timestamp

--- ClienteServidor/src/main/java/org/example/Controller/UserController.java
+++ ClienteServidor/src/main/java/org/example/Controller/UserController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Set;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
--- ClienteServidor/src/main/java/org/example/Repository/UserRepository.java
+++ ClienteServidor/src/main/java/org/example/Repository/UserRepository.java
@@ -1,6 +1,7 @@
 package org.example.Repository;
 
 
+import java.util.Optional;
 import org.example.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
