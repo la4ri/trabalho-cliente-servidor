652e2c82ddd2919380de191d7d8dc7a000d032f2
Larissa <larissamoreiracarvalho@gmail.com>
2024-12-12 22:33:43 -0300

    fix/timestamp

--- ClienteServidor/src/main/java/org/example/Repository/UserRepository.java
+++ ClienteServidor/src/main/java/org/example/Repository/UserRepository.java
@@ -3,6 +3,7 @@ package org.example.Repository;
 
 import org.example.Model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
 }
